b6d31cd2c9b91afd58a34031a2d38bf7ad171735
Siphamandla Zolakhe <zwelakhespha@gmail.com>
2024-08-27 18:11:45 +0200

    Initial Project Files with Models

---
+++ Online.Shopping.CouponsApi/Models/Coupon.cs
@@ -0,0 +1,10 @@
+﻿namespace Online.Shopping.CouponsApi.Models
+{
+    public class Coupon
+    {
+        public int CouponId { get; set; }
+        public string CouponCode { get; set; } = string.Empty;
+        public double DiscountAmount { get; set; }
+        public int MinimumAmount { get; set; }
+    }
+}
---
+++ Online.Shopping.CouponsApi/Models/Dtos/CouponDto.cs
@@ -0,0 +1,10 @@
+﻿namespace Online.Shopping.CouponsApi.Models.Dtos
+{
+    public class CouponDto
+    {
+        public int CouponId { get; set; }
+        public string CouponCode { get; set; } = string.Empty;
+        public double DiscountAmount { get; set; }
+        public int MinimumAmount { get; set; }
+    }
+}
