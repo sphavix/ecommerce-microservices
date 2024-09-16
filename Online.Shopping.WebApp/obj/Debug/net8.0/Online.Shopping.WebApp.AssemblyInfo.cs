590442f18ba270becba9503442d8adab72d3db87
Siphamandla Zolakhe <zwelakhespha@gmail.com>
2024-09-16 19:07:21 +0200

    Added Auth Dtos

--- Online.Shopping.WebApp/obj/Debug/net8.0/Online.Shopping.WebApp.AssemblyInfo.cs
+++ Online.Shopping.WebApp/obj/Debug/net8.0/Online.Shopping.WebApp.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Online.Shopping.WebApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+00b85c42a36afadcbf29fdc92e818460645acaf7")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("Online.Shopping.WebApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Online.Shopping.WebApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
